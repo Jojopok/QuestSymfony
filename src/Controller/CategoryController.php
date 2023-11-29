2973f817b18843351206791ac955450c2fc7ccb0
Jojopok <zaiajonathan@gmail.com>
2023-11-29 17:10:55 +0100

    Add for quest 10

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use App\Repository\ProgramRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+
+#[Route('/category', name: 'category_')]
+Class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render(
+            'category/index.html.twig',
+            ['categories' => $categories]
+        );
+    }
+
+    #[Route('/{categoryName}', name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        // Récupérer l'objet Category par le nom
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+
+        // Vérifier si la catégorie existe, sinon retourner une erreur 404
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        // Récupérer les séries appartenant à la catégorie, au maximum 3, ordonnées par ID décroissant
+        $series = $programRepository->findBy(
+            ['category' => $category],
+            ['id' => 'DESC'],
+            3
+        );
+
+        // Rendre la vue avec les séries et la catégorie
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'series' => $series,
+        ]);
+    }
+}
