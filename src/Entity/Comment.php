e056f86f04e29784bcc579419abcb1b34719425c
Jojopok <zaiajonathan@gmail.com>
2023-12-29 14:11:31 +0100

    add comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
@@ -14,18 +15,52 @@ class Comment
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $author = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Episode $episode = null;
+
     #[ORM\Column(type: Types::TEXT)]
     private ?string $comment = null;
 
     #[ORM\Column]
+    #[Assert\Range(
+        min: 0,
+        max: 10,
+        notInRangeMessage: 'You must be between {{ min }}cm and {{ max }}cm tall to enter',
+    )]
     private ?int $rate = null;
-
-    #[ORM\ManyToOne(inversedBy: 'comments')]
-    private ?Episode $episode = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getEpisode(): ?Episode
+    {
+        return $this->episode;
+    }
+
+    public function setEpisode(?Episode $episode): static
+    {
+        $this->episode = $episode;
+
+        return $this;
     }
 
     public function getComment(): ?string
@@ -48,18 +83,6 @@ class Comment
     public function setRate(int $rate): static
     {
         $this->rate = $rate;
-
-        return $this;
-    }
-
-    public function getEpisode(): ?Episode
-    {
-        return $this->episode;
-    }
-
-    public function setEpisode(?Episode $episode): static
-    {
-        $this->episode = $episode;
 
         return $this;
     }
